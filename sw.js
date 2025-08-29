93bcdc96f67d3a8f2fa4db514cd6c54e65e9f009
vimalRaj45 <vimalraj5207@gmail.com>
2025-08-29 22:07:21 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,30 @@
-// Install event
-self.addEventListener('install', () => self.skipWaiting());
+console.log('SW.js loaded');
 
-// Activate event
+self.addEventListener('install', () => {
+  console.log('SW installed');
+  self.skipWaiting();
+});
+
 self.addEventListener('activate', event => {
+  console.log('SW activated');
   event.waitUntil(self.clients.claim());
 });
 
-// Handle notification click
 self.addEventListener('notificationclick', event => {
+  console.log('Notification clicked:', event.notification);
   event.notification.close();
 
   const url = event.notification.data?.url || 'https://vsgroupsofcompany.neocities.org/vs';
   event.waitUntil(
     self.clients.matchAll({ type: 'window', includeUncontrolled: true }).then(windowClients => {
-      // Focus an existing tab if open
       for (const client of windowClients) {
         if (client.url === url && 'focus' in client) {
+          console.log('Focusing existing window:', client.url);
           return client.focus();
         }
       }
-      // Otherwise, open a new tab
       if (self.clients.openWindow) {
+        console.log('Opening new window:', url);
         return self.clients.openWindow(url);
       }
     })
